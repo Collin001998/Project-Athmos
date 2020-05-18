1d51850ece86739cb075b8c1e7b906626d837737
Collin <Collin001998@users.noreply.github.com>
2020-05-18 14:39:52 +0200

    Player Can Enter Level
    - added hub level gate
    - trigger collision hub

---
+++ Assets/HubLevelGate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubLevelGate : MonoBehaviour
+{
+    public int level;
+    public string prefixSceneLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player>())
+        {
+            MoveToScene(prefixSceneLevel,level);
+        }
+    }
+
+    void MoveToScene(string prefix, int level)
+    {
+        SceneManager.LoadScene(prefix+level);
+    }
+}
--- Assets/Scripts/charMovement.cs
+++ Assets/Scripts/charMovement.cs
@@ -15,8 +15,10 @@ public class charMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
-        { 
+        {
+            
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             int layerMask = LayerMask.GetMask("Floor");
