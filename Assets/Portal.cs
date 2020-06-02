3ff6514707e59f7668b17089763ff004cd1d8fb1
Collin <Collin001998@users.noreply.github.com>
2020-06-02 22:31:18 +0200

    gate
    - plateau
    - gate checks
    - progress
    
    still needs cleaning/polishing
    and UI

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -1,35 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool randomOrder;
     public List<GameObject> plate;
-    private List<GameObject> progress;
+    public List<GameObject> progress;
+    private bool[] completed;
     void Start()
     {
-        
+        completed = new bool[plate.Count];
+        for(int i = 0; i <= plate.Count - 1; i++)
+        {
+            completed[i] = false;
+        }
+        Debug.Log(completed.Length);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CheckCompleted())
+        {
+            Debug.Log("level competed");
+            Player player = other.gameObject.GetComponent<Player>();
+            player.FinishedLevel();
+        }
+        if (!CheckCompleted())
+        {
+            Debug.Log("level not completed");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void ResetProgress()
+    public void CheckProgress()
     {
-        progress.Clear();
+        if(plate[progress.Count -1] == progress[progress.Count -1])
+        {
+            Debug.Log("correct plate.");
+            completed[progress.Count -1] = true;
+            plate[progress.Count - 1].GetComponent<plateau>().activated = true;
+        }
+        else
+        {
+            Debug.Log("correct plate.");
+            progress.Clear();
+            foreach(GameObject gameObject in plate)
+            {
+                gameObject.GetComponent<plateau>().activated = false;
+            }
+        }
     }
-
-    public void AddProgress(GameObject plate)
+    public bool CheckCompleted()
     {
-        progress.Add(plate);
-    }
-    public void CheckProgress(GameObject plate)
-    {
+        bool canEnterPortal = true;
 
+        for (int i = 0; i < completed.Length; i++)
+        {
+            if(completed[i] == false)
+            {
+                canEnterPortal = false;
+            }
+        }
+
+        return canEnterPortal;
     }
 }
---
+++ Assets/plateau.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plateau : MonoBehaviour
+{
+    public bool activated = false;
+    public GameObject portal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!activated)
+        {
+            portal.GetComponent<Portal>().progress.Add(this.gameObject);
+            portal.GetComponent<Portal>().CheckProgress();
+        }
+    }
+}
